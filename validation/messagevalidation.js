cb4c8c9a29dad3e9bdd212617b20e63c846dc6ca
ABLEN93 <iamme5643@gmail.com>
2021-11-02 14:59:09 +0100

    update f stack App

---
+++ validation/messagevalidation.js
@@ -0,0 +1,19 @@
+import { body } from "express-validator";
+
+// This defines validation and sanitation for our message
+// It specifically examines the body of a request and makes sure
+// that if there is a message inside, the contents of the message is OK
+export const messagerules = [
+  body("message").isLength({ min: 0, max: 120 }).withMessage("message-length"),
+  body("message").isAlphanumeric("de-DE").withMessage("message-invalid"),
+  body("message").trim(),
+  body("message").blacklist("e"),
+];
+
+
+// this isEmail method check if @ is exist
+export const emailrules = [
+  body("email").isEmail().withMessage("message-length"),
+
+];
+
